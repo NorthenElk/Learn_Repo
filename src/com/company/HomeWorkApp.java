cebcd22f9e05e34462b5c6dcd90b6c6a2a0ee767
ponomarev <ponomandrey@gmail.com>
2022-05-23 11:59:10 +0300

    First lesson- done.

---
+++ src/com/company/HomeWorkApp.java
@@ -0,0 +1,48 @@
+package com.company;
+
+class HomeWorkApp {
+    public static void main(String [] args) {
+        printThreeWords();
+        checkSumSign(4, 2, 6);
+        printColor(200);
+        compareNumbers( 300, -28);
+    }
+    public static void printThreeWords(){
+        System.out.println("Organe");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+    public static void checkSumSign (int a, int b, int c){
+        a = 4;
+        b = 2;
+        c = a + b;
+        if (c >= 0) {
+            System.out.println("Сумма положительная");
+        }
+        else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+    public static void printColor(int value){
+        value = 200;
+        if (value <= 0){
+            System.out.println("Красный");
+        }
+        else if (0 < value  && value <= 100) {
+            System.out.println("Желтый");
+        }
+        else if (value > 100){
+            System.out.println("Зеленый");
+        }
+    }
+    private static void compareNumbers (int a, int b){
+        a =300;
+        b = -28;
+        if  (a >= b){
+            System.out.println("a >= b");
+        }
+        else{
+            System.out.println("a < b");
+        }
+    }
+}
