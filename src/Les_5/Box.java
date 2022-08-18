138bd4e3d4035726804432fcd02d3c21a823fb89
NorthenElk <105703237+NorthenElk@users.noreply.github.com>
2022-08-18 15:50:38 +0300

    Update Box.java
    Убрал кoмент.

--- src/Les_5/Box.java
+++ src/Les_5/Box.java
@@ -3,41 +3,6 @@ package Les_5;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-/*public class Box<T extends Fruit> {
-
-    private ArrayList<T> items;
-
-    public Box (T items){
-        this.items = new ArrayList<>(Arrays.asList(items));
-    }
-    public void add(T items){
-        this.items.addAll((Arrays.asList(items)));
-    }
-    public void remove(T items){
-        for (T item: items) this.items.remove(item);
-    }
-    public ArrayList<T> getFruits(){
-        return new ArrayList<>(items);
-    }
-    public void clear(){
-        items.clear();
-    }
-
-    float getWeight(){
-        if (items.size() == 0) return 0;
-        float weight = 0;
-        for (T fruit: items) weight += fruit.getWeight();
-        return weight;
-    }
-    public boolean compare(Box){
-        return this.getWeight() == box.getWeight();
-    }
-    public void move(Box<? super T> box){
-        box.items.addAll(this.items);
-        clear();
-    }
-}*/
-
 public class Box<T extends Fruit> {
     private final ArrayList<T> items;
 
