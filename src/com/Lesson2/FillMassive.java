d3b61217abc17bacaf350497ccc8031c85ccd187
a.ponomarev <ponomandrey1@gmail.com>
2022-08-19 16:44:48 +0300

    Обобщения повторить(Box.java)

--- src/com/Lesson2/FillMassive.java
+++ src/com/Lesson2/FillMassive.java
@@ -7,10 +7,10 @@ public class FillMassive {
     public static void createArray(){
         int[] arr = new int[100];
         int counter = 0;
-        for (int i = 1; i < arr.length; i++) {
-            counter = counter +1;
+        for (int i = 1; i <= arr.length; i++) {
+            counter = counter + 1;
             System.out.print(counter + " ");
-            }
+        }
         System.out.println();
     }
 }
