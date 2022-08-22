59fb4585480e1aae0d0b9af4cfd8d6dd60528c99
a.ponomarev <ponomandrey1@gmail.com>
2022-08-22 14:45:31 +0300

    небольшие правки

--- src/com/Lesson2/Sum.java
+++ src/com/Lesson2/Sum.java
@@ -5,9 +5,9 @@ public class Sum {
         amount(8, 12, 20);
     }
 
-    public static void amount(int a, int b, int sum) {
+    public static void/*int, if return in the end*/ amount(int a, int b, int sum) {
         a = 8;
-        b = 12;
+        b = 9;
         sum = a + b;
         switch (sum) {
             case 10:
@@ -25,7 +25,27 @@ public class Sum {
                 break;
             default:
                 System.out.println(false);
-                break;
         }
+        /*switch (sum) {
+            case 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 -> System.out.println(true);
+            default -> System.out.println(false);
+        }*/
+       /* switch (sum) {
+            case 17:
+            case 10:
+            case 11:
+            case 12:
+            case 13:
+            case 14:
+            case 15:
+            case 16:
+            case 18:
+            case 19:
+            case 20:
+                System.out.println(true);
+            default:
+                System.out.println(false);
+        }
+        return sum;*/
     }
 }
