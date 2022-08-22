b262d46666b0e7eac75d59fad19e0c705e160536
a.ponomarev <ponomandrey1@gmail.com>
2022-08-22 14:33:29 +0300

    небольшие правки

--- src/Lesson_4/Payment.java
+++ src/Lesson_4/Payment.java
@@ -1,4 +1,19 @@
 package Lesson_4;
 
 public class Payment {
+    public static void main(String[] args) {
+
+    }
+
+    static class SalesItem {
+        public void main(String[] args) {
+            sale();
+        }
+
+        public <T, saleItem> void sale() {
+            SalesItem Sony = new SalesItem();
+            SalesItem Sega = new SalesItem();
+            
+        }
+    }
 }
