8c47d622423a39d3d3cbcd3274f2d91f40fc2c72
a.ponomarev <ponomandrey1@gmail.com>
2022-08-22 16:59:02 +0300

    небольшие правки

---
+++ src/Les_6/TwoDStringArray.java
@@ -0,0 +1,13 @@
+package Les_6;
+
+public class TwoDStringArray {
+    public static void main(String[] args) {
+        twoDStringArray();
+
+    }
+
+    public static void twoDStringArray() {
+        int[][] arr = new int[4][4];
+        
+    }
+}
