20111ba1d2072f7675ad60c42f3aeb4eafb688a4
a.ponomarev <ponomandrey1@gmail.com>
2022-08-23 16:21:47 +0300

    небольшие правки

--- src/com/Lesson2/NumericalPositivity.java
+++ src/com/Lesson2/NumericalPositivity.java
@@ -9,17 +9,16 @@ class NumericalPositivity {
         testTern(55);
     }
 
-    public static void number(int x) {
-        if (x >= 0) {
-            System.out.println(x + "- положительное число!");
+    public static void number(int object) {
+        if (object >= 0) {
+            System.out.println(object + "- положительное число!");
         } else {
-            System.out.println(x + "- отрицательное число!");
+            System.out.println(object + "- отрицательное число!");
         }
     }
 
     static void testTern(int num) {  // тернарный оператор
         String wrd = (num < 0) ? "отрицательное" : "положительное";
-        System.out.printf("%d %s число!%n", num, wrd); // %d-int  %s-str
+        System.out.printf("%d %s число!%n", num, wrd);  /*%d-int  %s-str*/
     }
-
 }
