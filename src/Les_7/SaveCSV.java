2ecf468f34adc0c2e73e1c899cc235919726ca4f
a.ponomarev <ponomandrey1@gmail.com>
2022-08-25 16:57:06 +0300

    Начало работы со строками

---
+++ src/Les_7/SaveCSV.java
@@ -0,0 +1,14 @@
+package Les_7;
+/*Реализовать сохранение данных в csv файл;
+Реализовать загрузку данных из csv файла. Файл читается целиком*/
+
+public class SaveCSV {
+    public static void main(String[] args) {
+        saveSCV();
+    }
+
+    private static void saveSCV() {
+        StringBuilder stringBuilder = new StringBuilder();
+
+    }
+}
