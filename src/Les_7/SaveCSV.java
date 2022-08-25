8a935d1ddbd37b745a8bfc77eb42e5c108bba592
a.ponomarev <ponomandrey1@gmail.com>
2022-08-25 17:57:58 +0300

    Начало работы со строками

--- src/Les_7/SaveCSV.java
+++ src/Les_7/SaveCSV.java
@@ -1,14 +1,28 @@
 package Les_7;
 /*Реализовать сохранение данных в csv файл;
-Реализовать загрузку данных из csv файла. Файл читается целиком*/
+Реализовать загрузку данных из csv файла. Файл читается целиком.*/
+
+import java.io.*;
 
 public class SaveCSV {
-    public static void main(String[] args) {
-        saveSCV();
-    }
-
-    private static void saveSCV() {
-        StringBuilder stringBuilder = new StringBuilder();
-
+    public static void main(String[] args) throws Exception {
+//            try {
+//                File file = new File("C:\\Users\\a.ponomarev\\Documents\\AQA Java\\Проект\\src\\test\\target\\Wow.txt");
+//                InputStream inputStream =new FileInputStream(file);
+//                byte[] arr = inputStream.readAllBytes();
+//                System.out.println(Arrays.toString(arr));
+//            }catch (Exception ex){
+//                System.out.println(Arrays.toString(ex.getStackTrace()));
+//            }finally {
+//                System.out.println("Inaff");
+//            }
+        File file = new File("C:\\Users\\a.ponomarev\\Documents\\AQA Java\\Проект\\src\\test\\target\\Wow.txt");
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+            String currenLine = reader.readLine();
+            System.out.println(currenLine);
+        } catch (FileNotFoundException e) {
+            System.out.println("Error 505");
+        }
     }
 }
