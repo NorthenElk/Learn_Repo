41f959d3674ad1d8731c1fa908a01e7624ad0c3a
a.ponomarev <ponomandrey1@gmail.com>
2022-08-29 10:49:28 +0300

    Работа со строками

--- src/Les_7/SaveCSV.java
+++ src/Les_7/SaveCSV.java
@@ -3,19 +3,10 @@ package Les_7;
 Реализовать загрузку данных из csv файла. Файл читается целиком.*/
 
 import java.io.*;
+import java.util.Arrays;
 
 public class SaveCSV {
     public static void main(String[] args) throws Exception {
-//            try {
-//                File file = new File("C:\\Users\\a.ponomarev\\Documents\\AQA Java\\Проект\\src\\test\\target\\Wow.txt");
-//                InputStream inputStream =new FileInputStream(file);
-//                byte[] arr = inputStream.readAllBytes();
-//                System.out.println(Arrays.toString(arr));
-//            }catch (Exception ex){
-//                System.out.println(Arrays.toString(ex.getStackTrace()));
-//            }finally {
-//                System.out.println("Inaff");
-//            }
         File file = new File("C:\\Users\\a.ponomarev\\Documents\\AQA Java\\Проект\\src\\test\\target\\Wow.txt");
         try {
             BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
@@ -23,6 +14,52 @@ public class SaveCSV {
             System.out.println(currenLine);
         } catch (FileNotFoundException e) {
             System.out.println("Error 505");
+        }
+        bild_Of_String();
+        readFile();
+        read_To_Intermediate_Buffer();
+    }
+
+    public static void bild_Of_String() {
+        try (FileWriter writer = new FileWriter(
+                "C:\\Users\\a.ponomarev\\Documents\\AQA Java\\Проект\\src\\test\\target\\Wow.txt", false)
+        ) {
+            String entry = "Первая запись в проверочный файл. Добавиться или перезапишет?";
+            writer.write(entry);
+            writer.append('\n');
+            writer.append('E');
+
+            writer.flush();
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public static void readFile() {
+        try (FileReader reader = new FileReader(
+                "C:\\Users\\a.ponomarev\\Documents\\AQA Java\\Проект\\src\\test\\target\\Wow.txt")) {
+            int a;
+            while ((a = reader.read()) != -1) {
+                System.out.println((char) a);
+            }
+        } catch (IOException ex) {
+            System.out.print(ex.getMessage());
+        }
+    }
+
+    public static void read_To_Intermediate_Buffer() {
+        try (FileReader reader = new FileReader(
+                "C:\\Users\\a.ponomarev\\Documents\\AQA Java\\Проект\\src\\test\\target\\Wow.txt")) {
+            char[] buf = new char[256];
+            int a;
+            while ((a = reader.read(buf)) > 0) {
+                if (a < 256) {
+                    buf = Arrays.copyOf(buf, a);
+                }
+                System.out.println(buf);
+            }
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
         }
     }
 }
